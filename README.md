90e57875e37d917c01f59164c80c7def1ad9d517
harry <zhangjianfeng>
2020-08-27 14:03:05 +0800

    fix: add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+## WebSocket通信 学习项目
+`1、所用技术`
+
+    springboot2.1.4 + WebSocket + freemark实现简单的通信
+    
+`2、客户端连接测试`
+
+    访问 http://localhost:8080/testdemo/im/20
+    点击"开启socket"
+    在console上能看到连接成功
+
+`3、后台推送消息测试`
+
+    http://localhost:8080/testdemo/pushListToWeb?cid=20&message=444444
+    在客户端浏览器console上能看到消息
+    
+
+    
+    
--- src/main/java/com/websocket/demowebsocket/WebsocketDemoApplication.java
+++ src/main/java/com/websocket/demowebsocket/WebsocketDemoApplication.java
@@ -3,11 +3,14 @@ package com.websocket.demowebsocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Harry
+ */
 @SpringBootApplication
-public class DemowebsocketApplication {
+public class WebsocketDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemowebsocketApplication.class, args);
+        SpringApplication.run(WebsocketDemoApplication.class, args);
     }
 
 }
